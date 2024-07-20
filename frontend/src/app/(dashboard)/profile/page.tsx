5d5975bf048493a621b08ba62263d1495e7fa278
Tiltusha <elcoronabooking@gmail.com>
2024-07-20 18:13:10 +0300

    пара новых страниц добавлена а че)

--- frontend/src/app/(dashboard)/profile/page.tsx
+++ frontend/src/app/(dashboard)/profile/page.tsx
@@ -1,6 +1,7 @@
 import { authConfig } from "@/configs/auth";
 import ProtectedPage from '@/components/auth/ProtectedPage';
 import { getServerSession } from "next-auth/next"
+import LogoutButton from '@/components/auth/LogoutButton';
 
 export default async function Page() {
     const session = await getServerSession(authConfig);
@@ -10,6 +11,7 @@ export default async function Page() {
             <ProtectedPage>
                 <h1>Profile of {session?.user?.name} </h1>
                 {session?.user?.image && <img src={session?.user?.image} />}
+                <LogoutButton />
             </ProtectedPage>
         </div>
      )
