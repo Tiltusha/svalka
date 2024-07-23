868c5cd296a78bb3cdb3d8185247c5d791dc7a18
Tiltusha <elcoronabooking@gmail.com>
2024-07-23 19:11:33 +0300

    fix sidebar

--- frontend/src/components/layout/Nav/Nav.tsx
+++ frontend/src/components/layout/Nav/Nav.tsx
@@ -1,24 +1,28 @@
-'use client'
+'use client';
 
-import React from 'react'
-import styles from './nav.module.sass'
-import { useSession } from 'next-auth/react'
-import Link from 'next/link'
+import React from 'react';
+import styles from './nav.module.sass';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 function Nav() {
     const { data: session, status } = useSession();
 
     return (
         <nav className={styles.nav}>
-            <div className="logo">
-                {(status === 'unauthenticated' && <Link href="/"><p className={styles.text}>Svalka</p></Link>)}
+            <div className={styles.logo}>
+                {status === 'unauthenticated' && (
+                    <Link href="/">
+                        <p className={styles.text}>Svalka</p>
+                    </Link>
+                )}
             </div>
             <div className={styles.buttons}>
-                {(status === 'authenticated' && <button>Sign Out</button>)}
-                {(status === 'unauthenticated' && <button>Sign In</button>)}
+                {status === 'authenticated' && <button>Sign Out</button>}
+                {status === 'unauthenticated' && <button>Sign In</button>}
             </div>
         </nav>
-  )
+    );
 }
 
-export default Nav
+export default Nav;
