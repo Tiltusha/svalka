6766b79a61c7b22ee35bae93c93fe7294f14eb37
Tiltusha <elcoronabooking@gmail.com>
2024-07-26 14:56:13 +0300

    weather

--- frontend/src/app/weather/page.tsx
+++ frontend/src/app/weather/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ProtectedPage from '@/components/auth/ProtectedPage'
+import LocationFetcher from '@/components/weather/LocationFetcher'
 
 export default function page() {
   return (
     <section>
         <ProtectedPage>
-            Приложение погоды принято в разработку!
+            <h2>Weather</h2>
+            <LocationFetcher />
         </ProtectedPage>
     </section>
   )
